8abf54de344be85566480f08c755db0f01d3a96d
李先生 <11733074+li1198784239@user.noreply.gitee.com>
2024-09-16 20:49:31 +0800

    初始化项目

--- src/icons/index.js
+++ src/icons/index.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import SvgIcon from '@/components/SvgIcon'// svg component
 
 // register globally
-Vue.component('svg-icon', SvgIcon)
+Vue.component('svg-icon', SvgIcon) // 全局注册组件
 
+// 扫描目录中的文件
 const req = require.context('./svg', false, /\.svg$/)
+console.log(req.keys())
+// map循环每一项map(()=>{})
+// 循环引用到每一项
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
